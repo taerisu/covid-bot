042159ededcd8f886ef80b1b79ee045c451bae69
Zamur650 <Zamur_yt@mail.ru>
2021-07-30 09:35:10 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# covid-bot
+
+<p align="center">
+  <a href="" rel="noopener">
+ <img width=200px height=200px src="icon.png" alt="Bot logo"></a>
+</p>
+
+## Table of Contents
+
+- [About](#about)
+- [Getting Started](#getting_started)
+- [Usage](#usage)
+
+## About <a name = "about"></a>
+
+Discord bot that displays covid-19 data.
+
+## Getting Started <a name = "getting_started"></a>
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See [deployment](#deployment) for notes on how to deploy the project on a live system.
+
+### Prerequisites
+
+What things you need to install the software and how to install them.
+
+```
+node
+```
+
+### Installing
+
+Install dependencies
+
+```
+npm i
+```
+
+Write settings
+
+ as example
+[.env.example](.env.example)
+
+Start bot
+
+```
+npm run start
+```
+
+## Usage <a name = "usage"></a>
+
+Write *covid (country name / nothing for world)
+
+### Demo
+<p align="center">
+  <a href="demo.jpg" rel="noopener">
+ <img width=200px height=200px src="demo.jpg" alt="Bot logo"></a>
+</p>
---
+++ src/index.js
@@ -0,0 +1,52 @@
+const Discord = require('discord.js')
+const translate = require('@iamtraction/google-translate')
+const fetch = require('node-fetch')
+
+require('dotenv').config()
+
+const client = new Discord.Client()
+
+client.once('ready', () => {
+	console.log('Ready!')
+})
+
+client.on('message', async message => {
+  if (!message.content.startsWith(process.env.PREFIX) || message.author.bot) return
+	const args = message.content.slice(process.env.PREFIX.length).trim().split(' ')
+	const command = args.shift().toLowerCase()
+
+  if (command == 'covid') {
+    let responseAll = await (await fetch('https://api.covid19api.com/summary')).json()
+
+    // Make default request World
+    let response = responseAll.Global
+    response.Country = 'Мир'
+
+    // Make request valid via translate
+    let request = (await translate(args.join(' '), { to: "en" })).text.split(' ').join('-').toLowerCase()
+
+    // Find request
+    if (args) responseAll.Countries.forEach(country => {
+      if (country.Slug == request) response = country
+    })
+
+    // Send result
+		const Embed = new Discord.MessageEmbed()
+      .setColor(process.env.BOT_COLOR)
+      .setTitle(response.Country)
+      .setURL('https://xn--80aesfpebagmfblc0a.xn--p1ai/')
+      .addFields(
+        { name: 'Новых заражений', value: response.NewConfirmed, inline: true },
+        { name: 'Новых смертей', value: response.NewDeaths, inline: true },
+        { name: 'Новых вылечено', value: response.NewRecovered, inline: true },
+        { name: 'Всего заражений', value: response.TotalConfirmed, inline: true },
+        { name: 'Всего смертей', value: response.TotalDeaths, inline: true },
+        { name: 'Всего вылечено', value: response.TotalRecovered, inline: true }
+      )
+      .setTimestamp()
+
+    message.channel.send(Embed);
+	}
+})
+
+client.login(process.env.TOKEN)
